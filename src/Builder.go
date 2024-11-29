f922cce1ad1146eb5f8ff80b7e87fdb52876a6b0
hancj-dev <han@inhand.com.cn>
2024-11-29 17:39:30 +0800

    feat: Observer Pattern
    添加了观察者模式的实现，并在Builder模式中进行了相应的修改。更新了BestPractice.md文件以总结这些更改。

--- src/Builder.go
+++ src/Builder.go
@@ -1,93 +1,56 @@
 package main
 
-import "fmt"
-type Speed float64
-
-const (
-    MPH Speed = 1
-    KPH Speed = 1.60934
+import (
+	"github.com/your-project/car"
 )
 
-type Color string
-
-const (
-    BlueColor  Color = "blue"
-    GreenColor Color = "green"
-    RedColor   Color = "red"
-)
-
-type Wheels string
-
-const (
-    SportsWheels Wheels = "sports"
-    SteelWheels   Wheels = "steel"
-)
-
+// Builder interface defines the methods for constructing a car
 type Builder interface {
-    Color(Color) Builder
-    Wheels(Wheels) Builder
-    TopSpeed(Speed) Builder
-    Build() Interface
-}
-
-type Interface interface {
-    Drive() error
-    Stop() error
+	Paint(color car.Color) Builder
+	Wheels(wheels car.Wheels) Builder
+	TopSpeed(speed car.Speed) Builder
+	Build() Interface
 }
 
 type builder struct {
-    color  Color
-    wheels Wheels 
-    speed  Speed
+	color  car.Color
+	wheels car.Wheels
+	speed  car.Speed
+}
+
+func (b *builder) Paint(color car.Color) Builder {
+	b.color = color
+	return b
+}
+
+func (b *builder) Wheels(wheels car.Wheels) Builder {
+	b.wheels = wheels
+	return b
+}
+
+func (b *builder) TopSpeed(speed car.Speed) Builder {
+	b.speed = speed
+	return b
+}
+
+func (b *builder) Build() Interface {
+	return &car{
+		color:  b.color,
+		wheels: b.wheels,
+		speed:  b.speed,
+	}
 }
 
 func NewBuilder() Builder {
 	return &builder{}
 }
 
-func (b *builder) Color(color Color) Builder {
-	b.color = color
-	return b
-}
-func (b *builder) Wheels(wheels Wheels) Builder {
-	b.wheels = wheels
-	return b
-}	
-func (b *builder) TopSpeed(speed Speed) Builder {
-	b.speed = speed
-	return b
-}
-
-func (b *builder) Build() Interface {
-    return &car{
-        color:  b.color,
-        wheels: b.wheels,
-        speed:  b.speed,
-    }
-}
-
-type car struct {
-    color  Color
-    wheels Wheels
-    speed  Speed
-}
-
-func (c *car) Drive() error {
-    return nil
-}
-
-func (c *car) Stop() error {
-    return nil
-}
-
 func main() {
-	assembly := NewBuilder().Color(RedColor)
-
-	familyCar := assembly.Wheels(SportsWheels).TopSpeed(50 * MPH).Build()
+	assembly := NewBuilder().Paint(car.RedColor)
+	
+	familyCar := assembly.Wheels(car.SportsWheels).TopSpeed(50 * car.MPH).Build()
 	familyCar.Drive()
 	
-	sportsCar := assembly.Wheels(SteelWheels).TopSpeed(150 * MPH).Build()
+	sportsCar := assembly.Wheels(car.SteelWheels).TopSpeed(150 * car.MPH).Build()
 	sportsCar.Drive()
-	fmt.Println("Sports car built successfully")
-}
-
+} 
